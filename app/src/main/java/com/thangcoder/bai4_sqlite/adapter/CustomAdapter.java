70eef55a92f15f2b877b85cc264a477a10385cfb
Chien <cheng.cit@gmail.com>
2017-04-15 17:41:29 +0700

    Initial commit

---
+++ app/src/main/java/com/thangcoder/bai4_sqlite/adapter/CustomAdapter.java
@@ -0,0 +1,70 @@
+package com.thangcoder.bai4_sqlite.adapter;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.thangcoder.bai4_sqlite.R;
+import com.thangcoder.bai4_sqlite.model.Student;
+
+import java.util.List;
+
+/**
+ * Created by chien on 4/10/17.
+ */
+
+public class CustomAdapter extends ArrayAdapter<Student> {
+
+    private Context context;
+    private int resoure;
+    private List<Student> listStudent;
+
+    public CustomAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Student> objects) {
+        super(context, resource, objects);
+        this.context= context;
+        this.resoure=resource;
+        this.listStudent=objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ViewHolder viewHolder;
+        if(convertView==null){
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_list_student,parent,false);
+            viewHolder = new ViewHolder();
+            viewHolder.tvId = (TextView)convertView.findViewById(R.id.tv_id);
+            viewHolder.tvName = (TextView)convertView.findViewById(R.id.tv_name);
+            viewHolder.tvPhoneNumber = (TextView)convertView.findViewById(R.id.tv_phone_number);
+            viewHolder.tvEmail  = (TextView)convertView.findViewById(R.id.tv_email);
+            viewHolder.tvAddress = (TextView)convertView.findViewById(R.id.tv_address);
+
+            convertView.setTag(viewHolder);
+        }else{
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        Student student = listStudent.get(position);
+        viewHolder.tvId.setText(String.valueOf(student.getmID()));
+        viewHolder.tvName.setText(student.getmName());
+        viewHolder.tvAddress.setText(student.getmAddress());
+        viewHolder.tvEmail.setText(student.getmEmail());
+        viewHolder.tvPhoneNumber.setText(student.getmPhoneNumber());
+
+        return convertView;
+    }
+
+    public class ViewHolder{
+
+        private TextView tvId;
+        private TextView tvName;
+        private TextView tvAddress;
+        private TextView tvEmail;
+        private TextView tvPhoneNumber;
+    }
+}
